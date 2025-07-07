a70010c4e6f0d8584a04a0bc4ade580251f5fb67
tukioss <tulliopacerto@gmail.com>
2025-07-07 23:39:43 +0000

    Aggiunto script 'decisioni.py? con logica if/elif/else

---
+++ decisioni.py
@@ -0,0 +1,11 @@
+nome = input("Inserisci un nome: ")
+if  nome == "Tullio":
+    print ("Ciao Tullio! Il creatore dello script.")
+elif nome == "Fedez":
+    print ("Ciao Fedez! Il nome utente del server.")
+else:
+    print (f"Ciao, {nome}! Un nome nuovo, benvenuto!")
+print("Fine del programma.")
+
+
+
